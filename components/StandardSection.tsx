793d7f96d5daae7ddd8db0453c2050341902f5ac
Wayne Brown <themixengineer@gmail.com>
2026-04-04 22:26:11 +0700

    far update

--- components/StandardSection.tsx
+++ components/StandardSection.tsx
@@ -127,7 +127,7 @@ const StandardSection: React.FC = () => {
                 <div className="pt-6">
                   {/* Neon Border CTA */}
                   <div className="relative inline-block group p-[2px] rounded-full overflow-hidden transition-all duration-500 hover:scale-105 active:scale-95">
-                    <div className="absolute inset-[-1000%] animate-spin-slow bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"></div>
+                    <div className="absolute inset-[-1000%] animate-spin-slow bg-[conic-gradient(from_90deg_at_50%_50%,#7dd3fc_0%,#0369a1_50%,#7dd3fc_100%)]"></div>
                     <a 
                       href="https://buy.stripe.com/fZu3cv55bbKadeYa0b8bS01" 
                       className="relative block px-8 py-3.5 bg-neutral-950 text-neutral-200 rounded-full font-medium tracking-wide hover:bg-neutral-900 transition-colors text-center"
